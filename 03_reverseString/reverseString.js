09091410e9c2362aa62234eb40c7405ac47381b0
spalme21 <spalme21@gmail.com>
2023-07-13 22:34:18 -0500

    Finish 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const stringArray = string.split("");
+    let reversedArray = [];
+    for (let char of stringArray) {
+        reversedArray.unshift(char);
+    }
+    return reversedArray.join("");
 };
 
 // Do not edit below this line
