fbeb57c24cfe68d35c976c9e27a7d6b6a940ecb1
spalme21 <spalme21@gmail.com>
2023-07-14 19:14:21 -0500

    Finish 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if (a < 0 
+        || b < 0 
+        || typeof a != "number" 
+        || typeof b != "number") {
+        return "ERROR";
+    }
+    let sum = 0;
+    for (let i = Math.min(a, b); i <= Math.max(a, b); i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
