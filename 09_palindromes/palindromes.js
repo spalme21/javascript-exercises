2b3c984e07ab0042026b9017164c6452e3e403e7
spalme21 <spalme21@gmail.com>
2023-07-28 22:19:39 -0500

    Finish 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const arr = str.split("");
+    const letters = arr.filter(char => /^[a-zA-Z0-9]+$/.test(char));
+    const forward = letters.join("");
+    const reversed = letters.reverse().join("");
+    return reversed.toLowerCase() === forward.toLowerCase() ;
 };
 
 // Do not edit below this line
