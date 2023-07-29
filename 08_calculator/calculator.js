69fdbd5fdaf3bff1947ebd9cb8047e8075e9d42c
spalme21 <spalme21@gmail.com>
2023-07-28 22:01:20 -0500

    Finish 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((total, nextNum) => {
+    return total + nextNum;
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(a, b) {
+  const args = [...arguments];
+  return args.reduce((total, nextNum) => {
+    return total * nextNum;
+  }, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	const arr = Array.from(
+    {length: num},
+    (value, index) => index + 1
+  );
+  return arr.reduce((total, nextNum) => {
+    return total * nextNum;
+  }, 1);
 };
 
 // Do not edit below this line
